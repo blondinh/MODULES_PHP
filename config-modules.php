c26b7f01399b33180a7ace5d5958d5a8451c6ad0
blondinh <clement.orain@gmail.com>
2017-12-10 21:48:12 +0100

    new datas

--- config-modules.php
+++ config-modules.php
@@ -4,3 +4,6 @@
 define('Local_path', "C:\\DEVELOPMENT\\MODULES_PHP");
 define('Git_path', "C:\\Program Files\\Git\\cmd\\git.exe");
 define('Remote_name', 'origin');
+
+// Module Yandex XML
+define('IDS', array(1206, 1263, 1285, 1287, 1295, 327));
